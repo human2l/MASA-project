5acfd022efe3187be5bf913df37650453406b865
Kai <human2lkai@gmail.com>
2022-02-20 17:08:02 +1100

    MASA-client

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import { Footer as ArwesFooter, Paragraph } from "arwes";
+import Centered from "./Centered";
+
+const Footer = () => {
+  return (
+    <ArwesFooter animate>
+      <Centered>
+        <Paragraph style={{ fontSize: 14, margin: "10px 0" }}>
+          Created by Kai.
+        </Paragraph>
+      </Centered>
+    </ArwesFooter>
+  );
+};
+
+export default Footer;
